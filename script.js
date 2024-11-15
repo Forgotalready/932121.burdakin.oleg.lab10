60b9e40d66d0d7ae377bdb46edfe7f9cffcb8615
Forgotalready <oleg.burdakin2016@yandex.ru>
2024-11-15 12:51:25 +0700

    2

--- script.js
+++ script.js
@@ -8,9 +8,43 @@ document.querySelector(".curtain").addEventListener("click", (event) => {
     document.querySelector(".lamp").classList.remove("no-active")
 })
 
+var isOn = false
+
 document.querySelector(".lamp").addEventListener("click", (event) => {
     event.preventDefault()
     const light = document.querySelector(".light")
-    light.classList.remove("light-noactive")
-    light.classList.add("light-active")
+
+    if(isOn)
+    {
+        light.classList.remove("light-active")
+        light.classList.add("light-noactive")
+        document.querySelector(".focus-holder")
+            .style
+            .display = "none"   
+    }else{
+        light.classList.remove("light-noactive")
+        light.classList.add("light-active")
+        document.querySelector(".focus-holder")
+            .style
+            .display = "grid"    
+    }
+    isOn = !isOn
 })
+
+
+var number = 0
+const images = ["./Web - Лабораторная работа №1.10/Bird.png", "./Web - Лабораторная работа №1.10/Rabbit.png"]
+document.querySelector(".rabbit-main").addEventListener(
+    "click",
+    (event) => {
+        event.preventDefault()
+        elem = event.target
+        elem.classList.add("image-moving")
+        setTimeout(() => 
+            {
+                elem.src = images[number % 2]
+                elem.classList.remove("image-moving")
+                number++    
+            }, 2000)
+    }
+)
